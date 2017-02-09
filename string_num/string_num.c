1e463f638709bfe7cd38a6f4fe3797b6bce601cf
Arden Rasmussn <ardenisthebest@gmail.com>
2017-02-09 10:52:17 -0800

    Compleated

--- string_num/string_num.c
+++ string_num/string_num.c
@@ -4,17 +4,83 @@
 #include <string.h>
 
 int alphabet[128];
+int transtable[100][128];
+char *word;
+char *file_contents;
+int wordcount = 0;
 
-/*
- * int get_word_length()
- * int get_num_sym()
- * void print_trans_table()
- * void construct_trans_table()
- * */
-int get_word_length(char *word) { return (strlen(word)); }
+int GetLength(char *str) { return (strlen(word)); }
+
+void RunDFA() {
+  int current_state = 0;
+  int char_index = 0;
+  for (int i = 0; i < GetLength(file_contents); i++) {
+    char_index = file_contents[i];
+    if (char_index >= 65 && char_index <= 90) {
+      char_index += 32;
+    }
+    current_state = transtable[current_state][char_index];
+    if (current_state == GetLength(word) + 3) {
+      wordcount++;
+      current_state = 2;
+    }
+  }
+}
+
+void ConstructTransTable() {
+  for (int i = 0; i < 128; i++) {
+    for (int j = 0; j < GetLength(word) + 4; j++) {
+      if (i == ' ' || i == '.' || i == ',') {
+        if (j == GetLength(word) + 2) {
+          transtable[j][i] = GetLength(word) + 3;
+        } else {
+          transtable[j][i] = 2;
+        }
+      } else {
+        transtable[j][i] = 1;
+      }
+    }
+  }
+  int current_state = 2;
+  int current_char = 0;
+  int char_index = word[current_char];
+  transtable[0][char_index] = 3;
+  while (current_char < GetLength(word)) {
+    char_index = word[current_char];
+    transtable[current_state][char_index] = current_state + 1;
+    current_state++;
+    current_char++;
+  }
+  current_state++;
+  for (int i = 0; i < 128; i++) {
+    transtable[current_state][i] = 2;
+  }
+}
+
+void DisplayTransTable() {
+  char *line = " |";
+  for (int i = 0; i < 128; i++) {
+    if (alphabet[i] != 0) {
+      line[GetLength(line) + 1] = ' ';
+      line[GetLength(line) + 1] = i;
+    }
+  }
+  printf("%s\n", line);
+  line = "--------------------";
+  printf("%s\n", line);
+  for (int i = 0; i < GetLength(word) + 4; i++) {
+    line = "";
+    for (int j = 0; j < 128; j++) {
+      if (alphabet[j] != 0) {
+        line[GetLength(line) + 1] = ' ';
+        line[GetLength(line) + 1] = transtable[i][j];
+      }
+    }
+    printf("%i|%s\n", i, line);
+  }
+}
 
 int main(int argc, char *argv[]) {
-  char *file_contents;
   long int input_file_size;
   FILE *input_file = fopen(argv[1], "rb");
   fseek(input_file, 0, SEEK_END);
@@ -24,13 +90,20 @@ int main(int argc, char *argv[]) {
   fread(file_contents, sizeof(char), input_file_size, input_file);
   fclose(input_file);
   file_contents[input_file_size] = '\0';
-  char *word = argv[2];
-  // char trans_table[100][128];
+  word = argv[2];
+
   for (int i = 0; i < 128; i++) {
     alphabet[i] = 0;
   }
-  for (int i = 0; i < 2; i++) {
-    alphabet[0]++;
+  alphabet[' '] = -1;
+  alphabet['.'] = -1;
+  alphabet[','] = -1;
+  for (int i = 0; i < GetLength(word); i++) {
+    if (word[i] >= 65 && word[i] <= 90) {
+      alphabet[word[i] + 32]++;
+    } else {
+      alphabet[word[i] + 0]++;
+    }
   }
   int flag = 0;
   printf("\"");
@@ -46,8 +119,13 @@ int main(int argc, char *argv[]) {
   }
   printf("\n");
 
-  // DO STUFF HERE
+  ConstructTransTable();
+  DisplayTransTable();
+  RunDFA();
+
+  printf("\n\n");
 
   int count = 0;
   printf("The special word \"%s\" appaears %i times.\n", word, count);
+  return 0;
 }
--- string_num/string_num.cpp
+++ string_num/string_num.cpp
@@ -3,37 +3,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
+// Time: 1:08
 int alphabet[128];
 int transtable[100][128];
 std::string word;
-int wordchars = 0;
+std::string file_contents;
+int wordcount = 0;
 
-/*
- * int get_word_length()
- * int get_num_sym()
- * void print_trans_table()
- * void construct_trans_table()
- * */
+void RunDFA() {
+  int currentstate = 0;
+  int charindex = -1;
+  for (int i = 0; i < file_contents.size(); i++) {
+    charindex = int(file_contents[i]);
+    if (charindex >= 65 && charindex <= 90) {
+      charindex += 32;
+    }
+    currentstate = transtable[currentstate][charindex];
+    if (currentstate == word.size() + 3) {
+      wordcount++;
+      currentstate = 2;
+    }
+  }
+}
 
 void ConstructTransTable() {
   for (int i = 0; i < 128; i++) {
-    for (int j = 0; j < wordchars; j++) {
+    for (int j = 0; j < word.size() + 4; j++) {
       if (i == int(' ') || i == int('.') || i == int(',')) {
-        transtable[j][i] = 2;
+        if (j == word.size() + 2) {
+          transtable[j][i] = word.size() + 3;
+        } else {
+          transtable[j][i] = 2;
+        }
       } else {
         transtable[j][i] = 1;
       }
     }
   }
-}
-
-void LoadWordChars() {
-  for (int i = 0; i < 128; i++) {
-    if (alphabet[i] != 0) {
-      wordchars++;
-    }
+  int currentstate = 2;
+  int currentchar = 0;
+  transtable[0][int(word[currentchar])] = 3;
+  while (currentchar < word.size()) {
+    transtable[currentstate][int(word[currentchar])] = currentstate + 1;
+    currentstate++;
+    currentchar++;
   }
-  wordchars += 1;
+  currentstate++;
+  for (int i = 0; i < 128; i++) {
+    transtable[currentstate][i] = 2;
+  }
 }
 
 void DisplayTransTable() {
@@ -47,7 +65,7 @@ void DisplayTransTable() {
   printf("%s\n", line.c_str());
   line = std::string(100, '-');
   printf("%s\n", line.c_str());
-  for (int i = 0; i < wordchars; i++) {
+  for (int i = 0; i < word.size() + 4; i++) {
     line = "";
     for (int j = 0; j < 128; j++) {
       if (alphabet[j] != 0) {
@@ -60,7 +78,6 @@ void DisplayTransTable() {
 }
 
 int main(int argc, char *argv[]) {
-  std::string file_contents;
   std::ifstream file(argv[1]);
   if (file.is_open()) {
     getline(file, file_contents);
@@ -75,7 +92,11 @@ int main(int argc, char *argv[]) {
   alphabet[int('.')] = -1;
   alphabet[int(',')] = -1;
   for (int i = 0; i < word.size(); i++) {
-    alphabet[int(word[i])]++;
+    if (int(word[i]) >= 65 && int(word[i]) <= 90) {
+      alphabet[int(word[i]) + 32]++;
+    } else {
+      alphabet[int(word[i])]++;
+    }
   }
   int flag = 0;
   printf("\"");
@@ -91,13 +112,11 @@ int main(int argc, char *argv[]) {
   }
   printf("\n\n");
 
-  // DO STUFF HERE
-  LoadWordChars();
   ConstructTransTable();
   DisplayTransTable();
+  RunDFA();
+  printf("\n\n");
 
-  int count = 0;
-  // std::string str = "The special word \"" + word + "\" appears %i times.\n";
-  printf("The special word \"%s\" appears %i times.\n", word.c_str(), count);
-  // printf(str.c_str(), count);
+  printf("The special word \"%s\" appears %i times.\n", word.c_str(),
+         wordcount);
 }
